ed0eedbdafc6ad072aeb5a3fff410c94a93aebb8
Ruby <carefree0127@gmail.com>
2017-05-06 14:24:24 +0800

    調整顯示的資料格式

--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -16,6 +16,9 @@ namespace MVC5Course.Models
         
         [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
         public string ProductName { get; set; }
+
+        //整個網站都不顯示小數點的時候，可在此自訂顯示的格式
+        [DisplayFormat(DataFormatString="{0:N0}",ApplyFormatInEditMode=true)]
         public Nullable<decimal> Price { get; set; }
         public Nullable<bool> Active { get; set; }
         public Nullable<decimal> Stock { get; set; }
