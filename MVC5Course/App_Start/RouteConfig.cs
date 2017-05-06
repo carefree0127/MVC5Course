8c223c6499a3c055fe0baa82e2bbceed3ef39441
Ruby <carefree0127@gmail.com>
2017-05-06 11:49:49 +0800

    新增頁面與HelloController

--- MVC5Course/App_Start/RouteConfig.cs
+++ MVC5Course/App_Start/RouteConfig.cs
@@ -16,6 +16,7 @@ namespace MVC5Course
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
+                //url: "{action}/{controller}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
