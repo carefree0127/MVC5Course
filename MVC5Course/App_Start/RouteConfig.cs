1ba63f009fb1a63434dd311aaaec5484d20a6b3c
Ruby <carefree0127@gmail.com>
2017-05-13 14:48:25 +0800

    回復RouteConfig

--- MVC5Course/App_Start/RouteConfig.cs
+++ MVC5Course/App_Start/RouteConfig.cs
@@ -12,12 +12,12 @@ namespace MVC5Course
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");//Web From有很多檔案都是axd結尾
-            routes.IgnoreRoute("{resource}.aspx/{*pathInfo}");//對應投影片，可以修改路由偽裝成php.jsp...
+            //routes.IgnoreRoute("{resource}.aspx/{*pathInfo}");//對應投影片，可以修改路由偽裝成php.jsp...
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}.aspx/{id}",
-                //url: "{controller}/{action}/{id}",
+                //url: "{controller}/{action}.aspx/{id}", //要改回來才不會出現403Forbidden
+                url: "{controller}/{action}/{id}",
                 //url: "{action}/{controller}/{id}",
                 //可自訂URL，可把關鍵字.Metadata加在網址增加SEO
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
