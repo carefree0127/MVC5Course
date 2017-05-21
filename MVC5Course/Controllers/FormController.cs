faf3d8c88d6a8a5a8ca3dc0c0e4c2a1c515b4120
Ruby <carefree0127@gmail.com>
2017-05-21 10:04:19 +0800

    練習建立 FormController 與 Edit 動作方法編輯 ProductName 欄位內容、加入 htmlAttributes 參數到 TextBoxFor 欄位中、DisplayFormat 宣告日期格式

---
+++ MVC5Course/Controllers/FormController.cs
@@ -0,0 +1,42 @@
+﻿using MVC5Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers
+{
+    public class FormController : Controller
+    {
+        private FabricsEntities db = new FabricsEntities();
+
+        // GET: Form
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            //有傳參數就有ModelBinding
+            return View(db.Product.Find(id));
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id,FormCollection form)
+        {
+            //ModelBinding ModelState 優先權較高
+            var product = db.Product.Find(id);
+
+            if (TryUpdateModel(product, includeProperties: new string[] { "ProductName" }))
+            {//只針對ProductName作ModelBinding
+                db.SaveChanges();
+                return View("Index");
+            }
+
+            product = db.Product.Find(id);
+            return View(product);
+        }
+    }
+}
--- MVC5Course/Models/ValidationAttribute/商品名稱必須包含Ruby字串Attribute.cs
+++ MVC5Course/Models/ValidationAttribute/商品名稱必須包含Ruby字串Attribute.cs
@@ -15,6 +15,9 @@ namespace MVC5Course.Models.ValidationAttribute
 
         public override bool IsValid(object value)
         {
+            if (value == null) {
+                return true;
+            }
             var str = (string)value;
             return str.Contains("Ruby");
         }
